a16eab4b3532dbfe582713d331346bdce4f673fe
Angelo Rafael <angelorpt@gmail.com>
2020-12-06 12:03:53 -0400

    add | Novo menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,10 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Dívidas Clientes
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>Github</div>
       </q-toolbar>
     </q-header>
 
@@ -51,47 +51,17 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksData = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: 'Clientes',
+    caption: null,
+    icon: 'person',
+    to: '/clientes'
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: 'Dívidas',
+    caption: null,
+    icon: 'payments',
+    to: '/dividas'
   },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
 ];
 
 export default {
