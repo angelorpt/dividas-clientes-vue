609818c931c8e6a3589523e3ee9601db1f596fbd
Angelo Rafael <angelorpt@gmail.com>
2020-12-06 23:55:50 -0400

    add | Componente Button Menu

---
+++ src/components/Utils/Menu/ButtonMenu.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <q-btn :icon="icon" round flat >
+      <q-menu>
+        <q-list style="min-width: 150px">
+
+          <q-item clickable
+                  v-close-popup
+                  v-for="(item, index) in listaMenu"
+                  :color="item.color ? item.color : ''"
+                  :key="index"
+                  @click="$emit(item.emit, item.emit)" >
+
+            <q-item-section avatar>
+              <q-icon :name="item.icon" />
+            </q-item-section>
+
+            <q-item-section clickable>
+              {{item.label}}
+            </q-item-section>
+
+          </q-item>
+        </q-list>
+      </q-menu>
+    </q-btn>    
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: String,
+    listaMenu: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      listaMenu_inner: [
+        {
+          color   : 'primary',
+          icon    : 'send',
+          label   : 'titulo fds',
+          emit    : 'click',
+          tooltip : 'tooltip'
+        }
+      ]
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
