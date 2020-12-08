b01af7800183ce06f1170fbaa6e5660f0374a166
Angelo Rafael <angelorpt@gmail.com>
2020-12-08 18:19:16 -0400

    upd | Atualização do apontamento no axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,23 +1,8 @@
 import Vue   from 'vue'
 import axios from 'axios'
 
-const url_api = ({ host, pathname }) => {
-
-    // let lstPath = pathname.split('/');
-    switch (host) {
-        // case 'localhost:8080':
-        //     return 'http://localhost:8080/api';
-
-        // case 'localhost:8081':
-        //     return 'http://localhost:8081/api';
-
-        default:
-            return 'http://divida.epizy.com/api';
-    }
-}
-
 const Axios = axios.create({
-    baseURL: url_api(window.location),
+    baseURL: 'http://127.0.0.1:8000/api',
     timeout: 30000,
 });
 
