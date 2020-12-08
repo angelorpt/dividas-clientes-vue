1f5995fe25e69b082e1302f74923f888476547d5
Angelo Rafael <angelorpt@gmail.com>
2020-12-08 15:15:12 -0400

    upd | Atualização das Rota da API para diferentes ambientes

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -5,11 +5,11 @@ const url_api = ({ host, pathname }) => {
 
     // let lstPath = pathname.split('/');
     switch (host) {
-        case 'localhost:8080':
-            return 'http://localhost:8080/api';
+        // case 'localhost:8080':
+        //     return 'http://localhost:8080/api';
 
-        case 'localhost:8081':
-            return 'http://localhost:8081/api';
+        // case 'localhost:8081':
+        //     return 'http://localhost:8081/api';
 
         default:
             return 'http://divida.epizy.com/api';
