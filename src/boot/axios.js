5d4c5aff952c14e01aa34286feeda221c0197775
Angelo Rafael <angelorpt@gmail.com>
2020-12-08 15:10:19 -0400

    upd | Atualização das Rota da API para diferentes ambientes

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,8 +1,23 @@
 import Vue   from 'vue'
 import axios from 'axios'
 
+const url_api = ({ host, pathname }) => {
+
+    // let lstPath = pathname.split('/');
+    switch (host) {
+        case 'localhost:8080':
+            return 'http://localhost:8080/api';
+
+        case 'localhost:8081':
+            return 'http://localhost:8081/api';
+
+        default:
+            return 'http://divida.epizy.com/api';
+    }
+}
+
 const Axios = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: url_api(window.location),
     timeout: 30000,
 });
 
